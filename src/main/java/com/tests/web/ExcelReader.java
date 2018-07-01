37610f2b8c474e660de57876b906343735ae9019
abukowska <bukowska.joa@gmail.com>
2018-07-01 21:47:24 +0200

    naming refactor

--- src/main/java/com/tests/web/ExcelReader.java
+++ src/main/java/com/tests/web/ExcelReader.java
@@ -19,41 +19,42 @@ public class ExcelReader {
 		Workbook workbook = null;
 		try {
 			String filePath = "src//main//resources//Z1.xlsx";
-		    File file = new File(filePath);
-		    FileInputStream inputStream = new FileInputStream(file);
-		    
-		    if (filePath.endsWith(".xlsx")) {
-		    	workbook = new XSSFWorkbook(inputStream);
-		    }
-		    inputStream.close();
-		    
+			File file = new File(filePath);
+			FileInputStream inputStream = new FileInputStream(file);
+
+			if (filePath.endsWith(".xlsx")) {
+				workbook = new XSSFWorkbook(inputStream);
+			}
+			inputStream.close();
+
 		} catch (FileNotFoundException fnf) {
 			fnf.printStackTrace();
 		} catch (IOException ioe) {
 			ioe.printStackTrace();
 		}
-	    Sheet sheet = workbook.getSheet("Arkusz1");
+		Sheet sheet = workbook.getSheet("Arkusz1");
 
-	    Iterator<Row> rowIter = sheet.iterator();
-	    
-	    while(rowIter.hasNext()) {
-	        Row row = rowIter.next();
-	        for (int j = 1; j < 4; j++) {
-	        	
-	        	if(row.getCell(j) == null) continue;
-	        	
-	            if (j == 3) {
-	            	DataFormatter dataFormatter = new DataFormatter();
-	            	String cellStringValue = dataFormatter.formatCellValue(row.getCell(j));
-	            	System.out.print(cellStringValue); 
-	            } else {
-	            	System.out.print(row.getCell(j).getStringCellValue() +" | ");
-	            }
-	        }
-	        System.out.println();
-	    }
+		Iterator<Row> rowIter = sheet.iterator();
+
+		while (rowIter.hasNext()) {
+			Row row = rowIter.next();
+			for (int j = 1; j < 4; j++) {
+
+				if (row.getCell(j) == null)
+					continue;
+
+				if (j == 3) {
+					DataFormatter dataFormatter = new DataFormatter();
+					String cellStringValue = dataFormatter.formatCellValue(row.getCell(j));
+					System.out.print(cellStringValue);
+				} else {
+					System.out.print(row.getCell(j).getStringCellValue() + " | ");
+				}
+			}
+			System.out.println();
+		}
 	}
-	
+
 	public static void main(String[] args) {
 		readExcel();
 	}
